8ef1a150a1f052660bb71c7c703c7829f583c0af
blue_lim <chinapoopoo@gmail.com>
2017-06-06 12:56:33 +0900

    냐냐

--- src/pages/join/join.ts
+++ src/pages/join/join.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -9,11 +10,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class JoinPage {
 
   name: string = '';
+  country: string = '';
+  countries: Array<any> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   }
 
   ionViewDidLoad() {
+    let obj;
+    this.getJSON().subscribe(data => obj=data, error => console.log(error));
+    console.log(obj);
+    
   }
 
+  getJSON() {
+    return this.http.get("./assets/js/countries.json")
+      .map((res:any) => res.json())
+  }
 }
