7278b9e449c111d8ee8272d755ca609da6191f08
genesis <genesis041835@gmail.com>
2017-06-08 21:39:41 +0900

    Merge branch 'blue' of https://github.com/dps301/tplan into daeho2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { LoginPage } from '../pages/login/login';
-import { CoursePage } from '../pages/course/course';
 import { LoginPageModule } from '../pages/login/login.module';
 import { CoursePageModule } from '../pages/course/course.module';
-import { CourseDatailPageModule } from '../pages/course-datail/course-datail.module';
 import { CourseProvider } from '../providers/course/course';
 import { PlanProvider } from '../providers/plan/plan';
 import { CommonProvider } from '../providers/common/common';
@@ -17,7 +14,6 @@ import { HttpService } from '../services/http.service';
 import { HttpModule } from "@angular/http";
 import { ServerAddr } from '../services/server.addr';
 import { PlanPageModule } from '../pages/plan/plan.module';
-import { PlanItemComponent } from '../components/plan-item/plan-item';
 import { JoinPageModule } from '../pages/join/join.module';
 
 @NgModule({
